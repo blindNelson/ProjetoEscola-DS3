0ee845ab629093d16e87a82bac6fd685211d8e5a
blindNelson <rafalsilva5002@gmail.com>
2021-05-30 11:36:39 -0300

    DsI III : Integrado Front-end com Back-end

--- escola/src/components/ListagemAlunos/index.js
+++ escola/src/components/ListagemAlunos/index.js
@@ -2,15 +2,45 @@ import React, { Component } from 'react';
 import LinhaAluno from '../LinhaAluno';
 import './ListagemAlunos.css';
 
+/*
 const Alunos = [
     { 'ra': 11111, 'nome': 'André Silva Costa Santos Albuquerque', 'codCurso': 19},
     { 'ra': 22222, 'nome': 'Amanda', 'codCurso': 28},
     { 'ra': 33333, 'nome': 'Pedro', 'codCurso': 39},
     { 'ra': 44444, 'nome': 'Alice', 'codCurso': 59},
 ];
+*/
 
 export default class ListagemAlunos extends Component{
+    
+    constructor(props){
+        super(props);
+        this.state = {
+            dadosAlunos : []
+        };
+    }
+
+    componentDidMount() {
+        const apiUrl = "http://localhost:5000/api/aluno"
+
+        fetch(apiUrl)
+            .then(res => res.json)
+            .then(
+                (result) => {
+                    this.setState({
+                        dadosAlunos : result
+                    });
+                    console.log("buscaAlunos:" + result);
+                },
+                (error) =>{
+                    this.setState({ error })
+                }
+            )
+    }
+
     render(){
+        const {dadosAlunos} = this.state
+
         return(
         <div className="listagem">
             <h1 className="tituloListagem">Listagem de Alunos</h1>
@@ -22,7 +52,8 @@ export default class ListagemAlunos extends Component{
                         <th className="tabTituloCurso">Curso</th>
                     </tr>
                 </thead>
-                {Alunos.map((aluno) => <LinhaAluno ra={aluno.ra} nome={aluno.nome} codCurso={aluno.codCurso} />)}
+                {dadosAlunos.map((aluno) => <LinhaAluno ra={aluno.ra} nome={aluno.nome} codCurso={aluno.codCurso} />)}
+
             </table>
         </div>);
     }
